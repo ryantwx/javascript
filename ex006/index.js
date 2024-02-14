b849f1ef9340848de7be87d288e856c422d6984f
ryantwx <ryans2868@gmail.com>
2024-02-14 19:56:37 -0300

    multas

---
+++ ex006/index.js
@@ -0,0 +1,11 @@
+var vel = 0
+var v = vel *2.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel>40){
+    console.log(` Voce esta acima da via sua velocidade é ${vel} Km/H voce foi multado no valor de R$ ${v} `)
+}
+else{
+    console.log(` Diriga sempre usando cinto `)
+}
+if(vel<=20){console.log(`Voce esta parado mechase `)
+}
