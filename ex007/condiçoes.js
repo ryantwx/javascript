863b970423b2442110822b2162b2671775e42764
ryantwx <ryans2868@gmail.com>
2024-02-15 11:19:09 -0300

    condições

---
+++ ex007/condiçoes.js
@@ -0,0 +1,17 @@
+var idade = 12
+
+if(idade <16){
+    console.log(`Nao vota `)
+
+} 
+else
+
+    if(idade <18 || idade >=60){
+
+        console.log(`Voto opcional`)
+
+    }else{
+
+        console.log(` Voto obrigatorio`)
+
+    }
---
+++ ex007/semana.js
@@ -0,0 +1,37 @@
+var agora = new Date()
+var dia = agora.getDay()
+
+switch(dia){
+    case 0 :
+        console.log(`Domingo`)
+        break
+    
+    case 1 : 
+        console.log(`segunda`)
+        break
+
+    case 2 : 
+        console.log(`terça`)
+        break
+
+    case 3 : 
+        console.log(`quarta`)
+        break
+
+    case 4 : 
+        console.log(`quinta`)
+        break
+    
+    case 5 : 
+        console.log(`sexta`)
+        break
+
+    case 6 : 
+        console.log(`sabado`)
+        break
+    
+    default:
+        console.log(`Erro`)
+        break
+   
+}
