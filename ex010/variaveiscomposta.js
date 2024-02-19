5d8d54d3bc66ed1b4c5a6cb42db565051a8de664
ryantwx <ryans2868@gmail.com>
2024-02-19 16:58:49 -0300

    for in

---
+++ ex010/variaveiscomposta.js
@@ -0,0 +1,10 @@
+let num = [4,1,2 ,2,3]
+
+
+
+let n1 =num.indexOf(2)
+if(n1 == -1 ){
+    console.log(`O valor nao foi encontrado`)
+}else{
+    console.log(n1)
+}
---
+++ ex010/vetornatela.js
@@ -0,0 +1,8 @@
+let a =[1,5,8,4,9,2,6]
+
+// for(let pos = 0 ; pos<a.length ;pos++){
+//     console.log(`esse e o valor ${pos} na qual o vetor vale:: ${a[pos]}`)
+// }
+for(let pos in a){
+    console.log(`esse e o valor ${pos} na qual o vetor vale:: ${a[pos]}`)
+}
