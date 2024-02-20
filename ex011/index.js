a71418afb966773770e2fda644203ac195a4ea94
ryantwx <ryans2868@gmail.com>
2024-02-20 18:58:32 -0300

    treinando funções

---
+++ ex011/index.js
@@ -0,0 +1,22 @@
+
+// function parimp(n){
+//     if(n % 2 == 0){
+//         return 'par'
+//     }else{
+//         return'impar'
+//     }
+
+// }
+// let res = parimp(2)
+
+// console.log(`o numero é ${res}`)
+
+///////////////////////////////////////////////////////////////////////////////////
+function somar(n1 = 0 , n2 = 0){
+    return n1 + n2 
+}
+
+let res = somar(2 , 4)
+console.log(`${res}`)
+// Ou usamos  o seguinte 
+// console.log(somar(2,5))
